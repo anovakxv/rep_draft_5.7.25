7be82ed3e5fb6ff21cff5b194ebe118344ae90f3
anovakxv <202318642+anovakxv@users.noreply.github.com>
2025-06-24 15:05:19 -0600

    minor changes for hosting

--- my-ios-app/Python_Backend/your-app/create_user.py
+++ my-ios-app/Python_Backend/your-app/create_user.py
@@ -1,9 +1,16 @@
 from app import create_app, db
-from app.models.user import User
+from app.models.People_Models.User import User
 
 app = create_app()
 with app.app_context():
-    user = User(username='testuser', fname='Test', lname='User', email='test@example.com')
+    user = User(
+        username='testuser',
+        fname='Test',
+        lname='User',
+        email='test@example.com',
+        password='securepassword123',
+    )
     db.session.add(user)
     db.session.commit()
     print(f"Created user with ID: {user.id}")
+    

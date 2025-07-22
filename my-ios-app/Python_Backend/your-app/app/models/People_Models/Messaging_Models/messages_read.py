a95c8c675e3b46ea85171435fb5eb7323d337644
anovakxv <202318642+anovakxv@users.noreply.github.com>
2025-07-22 10:43:58 -0600

    same as above

--- my-ios-app/Python_Backend/your-app/app/models/People_Models/Messaging_Models/messages_read.py
+++ my-ios-app/Python_Backend/your-app/app/models/People_Models/Messaging_Models/messages_read.py
@@ -4,5 +4,4 @@ class MessagesRead(db.Model):
     __tablename__ = 'messages_read'
     id = db.Column(db.Integer, primary_key=True)
     users_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
-    messages_id = db.Column(db.Integer, db.ForeignKey('group_messages.id', ondelete="CASCADE"), nullable=False)
-    
+    messages_id = db.Column(db.Integer, db.ForeignKey('direct_messages.id', ondelete="CASCADE"), nullable=False)

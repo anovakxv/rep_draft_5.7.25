b46cc7eda1d4b1b28fb5f835ed430f960ebd46c9
anovakxv <202318642+anovakxv@users.noreply.github.com>
2025-07-28 20:13:56 -0600

    adding flask CORS for website input

--- my-ios-app/Python_Backend/your-app/app/__init__.py
+++ my-ios-app/Python_Backend/your-app/app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 from config import Config
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()
 socketio = SocketIO(cors_allowed_origins="*")
@@ -14,6 +15,7 @@ socketio = SocketIO(cors_allowed_origins="*")
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, origins=["https://networkedcapital.co"])
 
     db.init_app(app)
     socketio.init_app(app)

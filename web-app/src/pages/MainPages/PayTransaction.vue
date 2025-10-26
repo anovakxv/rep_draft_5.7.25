a7decf2dc368b7864e82a32ff07546cb985696cf
anovakxv <202318642+anovakxv@users.noreply.github.com>
2025-10-26 13:56:05 -0600

    web app

--- web-app/src/pages/MainPages/PayTransaction.vue
+++ web-app/src/pages/MainPages/PayTransaction.vue
@@ -125,6 +125,7 @@
         </div>
       </div>
     </div>
+    </div>
 
     <!-- Success Modal -->
     <div v-if="paymentStatus.status === 'success'" class="fixed inset-0 bg-black bg-opacity-60 z-50 flex items-center justify-center p-4">
